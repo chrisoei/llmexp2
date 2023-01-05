7a4f95716744ea21198d1476a49d1a3c8075ec57
Chris Oei <chris.oei@gmail.com>
2023-01-05 12:37:50 -0800

    Show CUDA stats.

--- exp1.py
+++ exp1.py
@@ -4,6 +4,14 @@ import time
 import torch
 import transformers
 
+# See https://stackoverflow.com/questions/58216000/get-total-amount-of-free-gpu-memory-and-available-using-pytorch
+def cudastats():
+    return {
+        "total_memory": torch.cuda.get_device_properties(0).total_memory,
+        "reserved_memory": torch.cuda.memory_reserved(0),
+        "allocated_memory": torch.cuda.memory_allocated(0)
+    }
+
 def hf(m1):
     return {
         "tokenizer": transformers.AutoTokenizer.from_pretrained(m1),
@@ -32,16 +40,20 @@ def infer(x, prompt1):
 modellist = [
     "facebook/galactica-125m",
     "Salesforce/codegen-350M-mono",
+    "bigscience/bloom-560m",
+    "bigscience/bloomz-560m",
     "facebook/galactica-1.3b",
     "Salesforce/codegen-2B-mono",
     "Salesforce/codegen-6B-mono",
     "facebook/galactica-6.7b",
-    "Salesforce/codegen-16B-mono"
+    "Salesforce/codegen-16B-mono",
+    "facebook/galactica-30b"
 ]
 
 for m1 in modellist:
     print(m1)
     x1 = hf(m1)
+    print(cudastats())
     t0 = time.time()
     print(infer(x1, "def helloworld():"))
     t1 = time.time()
